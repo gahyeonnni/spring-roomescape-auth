ed294a7c9c628506d10abecd8cfa8111e3804df5
gahyeonnni <hyeonnni@naver.com>
2026-05-21 18:47:07 +0900

    feat: store 테이블 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS reservation;
-DROP TABLE IF EXISTS "user";
 DROP TABLE IF EXISTS theme;
+DROP TABLE IF EXISTS store;
+DROP TABLE IF EXISTS "user";
 DROP TABLE IF EXISTS reservation_time;
 
 CREATE TABLE reservation_time
@@ -8,15 +9,6 @@ CREATE TABLE reservation_time
     id        BIGINT NOT NULL AUTO_INCREMENT,
     start_at  TIME   NOT NULL,
     finish_at TIME   NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE theme
-(
-    id          BIGINT       NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(255) NOT NULL,
-    description VARCHAR(500) NOT NULL,
-    image_url   VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -28,6 +20,26 @@ CREATE TABLE "user"
     password VARCHAR(255) NOT NULL,
     role     VARCHAR(50)  NOT NULL DEFAULT 'USER',
     PRIMARY KEY (id)
+);
+
+CREATE TABLE store
+(
+    id         BIGINT       NOT NULL AUTO_INCREMENT,
+    name       VARCHAR(255) NOT NULL,
+    manager_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (manager_id) REFERENCES "user" (id)
+);
+
+CREATE TABLE theme
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    image_url   VARCHAR(255) NOT NULL,
+    store_id    BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (store_id) REFERENCES store (id)
 );
 
 CREATE TABLE reservation
